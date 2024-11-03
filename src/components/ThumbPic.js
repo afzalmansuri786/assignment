6ea44d61825ade776714f7876a3872d806a67613
afzalmansuri786 <mansuri.mohammadafzal@gmail.com>
2024-11-03 23:16:18 +0530

    update

--- src/components/ThumbPic.js
+++ src/components/ThumbPic.js
@@ -1,18 +1,15 @@
-import React from "react";
-export class ThumbPic extends React.Component {
-    render(){
-        const {images , tab, myRef} =  this.props;
-        return(
-            <div className="thumb" ref={myRef}>
-                {
-                  images.map((img,index) => (
-                    <img src={img} alt={img} key={index}
-                      onClick={() => tab(index) }
-                    />
-                  ))
-                }
-              </div>
-        );
-    }
-}
+import React, { forwardRef } from "react";
+
+const ThumbPic = forwardRef(({ images, tab }, ref) => {
+    return (
+        <div className="thumb" ref={ref}>
+            {images.map((img, index) => (
+                <img src={img} alt={img} key={index}
+                    onClick={() => tab(index)}
+                />
+            ))}
+        </div>
+    );
+});
+
 export default ThumbPic;
